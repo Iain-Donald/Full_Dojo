4018592a48f8a47ab1fe0420f044315bc8404362
Iain Donald <iaindonald@outlook.com>
2022-04-21 12:01:30 -0700

    update

--- C#/ORMs/EFCore/Dishes/Dishes/Controllers/HomeController.cs
+++ C#/ORMs/EFCore/Dishes/Dishes/Controllers/HomeController.cs
@@ -35,5 +35,76 @@ namespace Dishes.Controllers
             
             return View(ALLDishes);
         }
+
+        [HttpGet("edit/{dishId}")]
+        public IActionResult EditDish(int dishId)
+        {
+            Dish viewDish = _context.Dishes.FirstOrDefault(dish => dish.DishId == dishId);
+
+            return View(viewDish);
+        }
+
+        [HttpPost("posteditdish/{dishId}")]
+        public IActionResult PostEditDish(Dish editedDish, int dishId)
+        {
+            Dish selectedDish = _context.Dishes.FirstOrDefault(dish => dish.DishId == dishId);
+            selectedDish.Name = editedDish.Name;
+            selectedDish.chefName = editedDish.chefName;
+            selectedDish.calories = editedDish.calories;
+            selectedDish.tastiness = editedDish.tastiness;
+            selectedDish.Description = editedDish.Description;
+            selectedDish.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("new")]
+        public IActionResult newDish()
+        {
+            return View();
+        }
+
+        [HttpGet("/{dishId}")]
+        public IActionResult viewDish(int dishId)
+        {
+            Dish selectedDish = _context.Dishes.FirstOrDefault(dish => dish.DishId == dishId);
+            return View(selectedDish);
+        }
+
+        [HttpPost("postnewdish")]
+        public IActionResult PostNewDish(Dish newDish)
+        {
+            newDish.CreatedAt = DateTime.Now;
+            newDish.UpdatedAt = DateTime.Now;
+
+            _context.Dishes.Add(newDish);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("update/{dishId}")]
+        public IActionResult UpdateDish(int dishId)
+        {
+            Dish selectedDish = _context.Dishes.FirstOrDefault(dish => dish.DishId == dishId);
+            selectedDish.Name = "New name";
+            selectedDish.UpdatedAt = DateTime.Now;
+        
+            _context.SaveChanges();
+        
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("delete/{dishId}")]
+        public IActionResult DeleteDish(int dishId)
+        {
+            Dish RetrievedDish = _context.Dishes.SingleOrDefault(dish => dish.DishId == dishId);
+
+            _context.Dishes.Remove(RetrievedDish);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
